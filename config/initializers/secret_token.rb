520c09fe26fe7c475d59f9c6076686be32c07abe
mnafian <semutliar2406@gmail.com>
2013-10-05 10:39:16 +0700

    Commit Pertama

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MahasiswaApp::Application.config.secret_key_base = '54db9fd1e6e43c6a03a07cdbfa131f63bd782f064720891d7e617262367937edbe81c7dc0d5d216b30077bc72b3b66c651fc95b70af50c3e9b595f3fbbc4ea3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MahasiswaApp::Application.config.session_store :cookie_store, key: '_Mahasiswa_App_session'
